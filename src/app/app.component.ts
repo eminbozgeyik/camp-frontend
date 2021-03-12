32a7d2db90922726c2f5f63d5147f41d260abe25
Mehmet Emin Bozgeyik <eminbozgeyik@hotmail.com>
2021-03-12 18:42:14 +0300

    new commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'northwind';
-  user  = 'Emin Bozgeyik';
+  title: string = 'northwind';
+  user: string = 'Engin Demiroğ';
   
 }
