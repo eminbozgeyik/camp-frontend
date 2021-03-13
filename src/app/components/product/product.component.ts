b49035282c29cd501012e8a89e41a503ee594bf2
Mehmet Emin Bozgeyik <eminbozgeyik@hotmail.com>
2021-03-13 20:23:56 +0300

    new commits

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductResponseModel } from 'src/app/models/productResponseModel';
@@ -10,19 +11,19 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
-  dataLoaded = false;
-  
-  constructor(private productService:ProductService) {}
+  apiUrl = 'https://localhost:44314/api/products/getall';
+
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
     this.getProducts();
   }
 
   getProducts() {
-    this.productService.getProducts().subscribe(response=>{
-      this.products = response.data
-      this.dataLoaded = true;
-    })
-     
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products = response.data;
+      });
   }
 }
--- src/app/models/productResponseModel.ts
+++ src/app/models/productResponseModel.ts
@@ -1,6 +1,6 @@
-import { Product } from "./product";
-import { ResponseModel } from "./responseModel";
+import { Product } from './product';
+import { ResponseModel } from './responseModel';
 
 export interface ProductResponseModel extends ResponseModel{
-    data:Product[]
+    data: Product[];
 }
